a1e3914f452c060369f7db94f691d3c2f76dbed7
Chris McC <mcclelland.c@gmail.com>
2020-12-30 15:10:04 +0000

    webpack-4

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-module.exports = function(source) {
-  var options = this.options.emberTemplatesLoader || {};
+var loaderUtils = require('loader-utils');
+
+module.exports = function (source) {
+  this.cacheable && this.cacheable();
+  var options = loaderUtils.getOptions(this);
+
   var precompile = options.precompile;
   if (!precompile) {
     precompile = require(options.compiler || './ember-template-compiler').precompile;
   }
 
-  this.cacheable && this.cacheable();
-
   return 'export default Ember.Handlebars.template(' + precompile(source) + ');';
 };
-
-module.exports.seperable = true;
